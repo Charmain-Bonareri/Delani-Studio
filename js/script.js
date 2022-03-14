157711dfd8ebcf0fd27e93dadf8e2da268bc52ea
Charmain <mogerecharmain@gmail.com>
2022-03-14 06:24:43 +0300

    This commit will add the hover effect over our overlays.

--- js/script.js
+++ js/script.js
@@ -32,4 +32,13 @@ let overlayTotalNumber =(
   $(".overlay").toArray().length
 );
 
-
+for(let overlayNumber = 1; overlayNumber<=overlayTotalNumber;overlayNumber++){
+  $(`#overlay${overlayNumber}`).mouseover(function() {
+  $(`#overlay${overlayNumber} > img`).addClass("overlay-effect");
+  $(`#work-overlay${overlayNumber}`).toggle();
+}).mouseout(function(){
+  $(`#overlay${overlayNumber} > img`).removeClass("overlay-effect");
+  $(`#work-overlay${overlayNumber}`).toggle();
+});
+}  
+});
